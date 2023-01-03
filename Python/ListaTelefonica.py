1e02e09e6f7580eb95ec9b16a5b75dff23edb896
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2023-01-02 21:49:07 -0300

    Create ListaTelefonica.py

---
+++ Python/ListaTelefonica.py
@@ -0,0 +1,85 @@
+class numero:
+    __lista=[str(i) for i in range(10)]
+    for i in range(ord("A"),ord("Z")+1):
+        __lista.append(chr(i))
+        
+    
+    def __init__(self,valor=None,base=10):
+        
+        if 0 < base < 37:
+            self.__base=base
+            if valor == None:
+                self.valor="0"
+            else:
+                self.valor=str(valor)
+        else:
+            raise ValueError(f"Base {base} invalida")
+    
+    @classmethod
+    def lista_posicao(cls,i):
+        return cls.__lista[i]    
+        
+    @property
+    def valor(self):
+        return self.__valor
+    
+    @valor.setter
+    def valor(self,valor):
+        self.__valor=[i for i in str(valor)]
+    
+    @classmethod
+    def __decimal(cls,valor,base):
+        d=0
+        for i,va in enumerate(valor[::-1]):
+            d+=cls.__lista.index(va)*base**i
+        return d
+        
+    def decimal(self,valor=None,base=None):
+        if valor == None:
+            valor=self.valor
+        if base == None:
+            base=self.__base
+        return self.__decimal(valor,base)
+        
+    def __str__(self):
+        
+        return "{"+f"valor:{''.join(self.__valor)},  base:{self.__base}"+"}"
+    
+    
+    def somar(self,soma):
+        
+        tamanho_s=len(soma)
+        v=self.valor[::-1]
+        while tamanho_s > len(v):
+            v.append("0")
+
+        soma=soma[::-1]
+        t=0
+        
+        for i in range(len(v)):
+           
+            if tamanho_s > i:
+                
+                t+=self.__decimal(soma[i],self.__base)+self.__decimal(v[i],self.__base)
+                if t < self.__base:
+                    v[i]=self.lista_posicao(t)
+                    
+                    t=0
+                else:
+                    t-=self.__base
+                    v[i]=self.lista_posicao(t)
+                    t=1
+        else:
+            if t != 0:
+                v.append(self.lista_posicao(t))
+        
+        return v[::-1]
+        
+    def __add__(self,*valores):
+        for v in valores:
+            if type(v) != int:
+                valor=v.valor
+            else:
+                valor=[i for i in str(v)]
+            self.__valor=self.somar(valor)
+        return self
